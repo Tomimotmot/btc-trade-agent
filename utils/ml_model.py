3a8cd029503bd212cfb990d26b4e09b6b5663865
Tomimotmot <reigl.tom@gmail.com>
2025-06-17 20:26:49 +0200

    Update ml_model.py

--- utils/ml_model.py
+++ utils/ml_model.py
@@ -3,6 +3,7 @@ import joblib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
